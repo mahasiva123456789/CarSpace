b278d33ba6d325c764a844ff439d0fb5301ffbe5
mahasiva123456789 <mahasiva2606@gmail.com>
2025-02-26 21:45:10 +0530

    Moved files into task1

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children, role }) => {
-    const userRole = localStorage.getItem("role");
-    return userRole === role ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
